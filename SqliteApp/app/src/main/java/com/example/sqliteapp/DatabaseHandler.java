7d69acd7a8a2ff3331c7baeb4437a527a1e5bec8
Thivija-Baskaramoorthy <thivibaskaran@gmail.com>
2023-10-14 07:42:02 +0530

    GPA Calculator

---
+++ SqliteApp/app/src/main/java/com/example/sqliteapp/DatabaseHandler.java
@@ -0,0 +1,31 @@
+package com.example.sqliteapp;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Student.db";
+    public static final String TABLE_NAME = "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "SURNAME";
+    public static final String COL_4 = "MARKS";
+    public  DatabaseHandler(@Nullable Context context) {
+
+        super (context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("create table "+ TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT, SURNAME TEXT, MARKS INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+            onCreate(sqLiteDatabase);
+    }
+}
