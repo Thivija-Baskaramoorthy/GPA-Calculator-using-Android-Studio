f2fbad18adbdb67f0d54ff37a055046f7a3afec9
Thivija-Baskaramoorthy <thivibaskaran@gmail.com>
2023-10-15 18:23:02 +0530

    2nd

--- app/src/main/java/com/example/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/gpacalculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.gpacalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to execute when the button is clicked
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
