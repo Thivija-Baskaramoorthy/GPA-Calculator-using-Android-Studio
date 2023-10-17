20000babef720e8708706538173b8990dfaf3cb9
Thivija-Baskaramoorthy <thivibaskaran@gmail.com>
2023-10-18 02:14:33 +0530

    Initial commit

--- app/src/main/java/com/example/gpacalculator/Semester2Activity.java
+++ app/src/main/java/com/example/gpacalculator/Semester2Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ThirdActivity extends AppCompatActivity {
+public class Semester2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_third);
+        setContentView(R.layout.activity_semester2);
     }
 }
